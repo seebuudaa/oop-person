b9dffa74b8fce872c57545a62847943aacd685bb
seebuudaa <sibusiso.mthimunye@umuzi.org>
2020-07-28 16:43:51 +0200

    adding special features to portfolio, linking

--- README.md
+++ README.md
@@ -21,6 +21,6 @@
 * create src and spec folders
 * src folder include actual file that has the code and spec folder contains jasmine spec file
 
-<img src="src/pic1.png" width="300"> 
+<img src="src/pic1.jpg" width="300"> 
  
 # happy coding!
--- src/person.js
+++ src/person.js
@@ -1,19 +1,55 @@
-"use strict"
 
-class Person {
-    constructor(name, age, gender, interests) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+
+const gender = document.getElementById('gender'),
+        enteredGender = gender.value,
+        interests = document.getElementById('interests'),
+        enteredInterests = interests.value,
+        form = document.getElementById('user-input'),
+        userNameInput = document.getElementById('name'),
+        enteredName = userNameInput.value,
+        ageInput = document.getElementById('age'),
+        enteredAge = ageInput.value;
+ 
+
+
+ class Person {
+    constructor(enteredName, enteredAge, enteredGender, interests) {
+        this.name = enteredName;
+        this.age = enteredAge;
+        this.gender = enteredGender;
         this.interests = interests
     }
     hello () {
         return `Hello, my name is ${this.name} and I am ${this.age} years old. My interests are ${this.interests}`;
     }
 }
-let person = new Person ("Ryan",30, "male", ["being a hardarse"," agile and"+ "  ssd hard drives"])
-let greeting = person.hello()
+let person = new Person (enteredName, enteredAge, enteredGender, [enteredInterests + " and  ssd hard drives"]),
+    greeting = person.hello();
+
+
+
+
+
+
 console.log(greeting)
 
 
-module.exports =  Person  ;
+
+function signupHandler(event) {
+  event.preventDefault();
+  const user = {
+    name: enteredName,
+    age: enteredAge,
+    gender: enteredGender,
+    interests: enteredInterests
+  };
+
+  document.getElementById("user").innerHTML = greeting
+ 
+}
+
+form.addEventListener('submit', signupHandler);
+
+//  module.exports =  Person  ;
+
+
